3933f2d2538b464650b1a851067963ddf7b36479
bince25 <incebaran25@gmail.com>
2022-06-16 19:00:30 +0300

    Kamera Takip

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    [Range(1, 10)]
+    public float smoothNumber;
+
+    void Update()
+    {
+        Follow();
+    }
+
+    private void Follow()
+    {
+        Vector3 targetPos = target.position + offset;
+        Vector3 smoothPos = Vector3.Lerp(this.transform.position, targetPos, smoothNumber * Time.deltaTime);
+
+        this.transform.position = smoothPos;
+
+    }
+}
