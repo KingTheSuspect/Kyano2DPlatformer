901cdf7cd2ed8fea21cf313814bb65dd690676ca
bince25 <incebaran25@gmail.com>
2022-07-12 22:41:20 +0300

    player clone

--- Assets/Scripts/CharacterSwitchController.cs
+++ Assets/Scripts/CharacterSwitchController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CharacterSwitchController : MonoBehaviour
 {
     private GameObject player;
+    private GameObject evilPlayer;
+    private GameObject mainCamera;
 
     public GameObject NormalMap;
     public GameObject EvilMap;
@@ -14,6 +16,7 @@ public class CharacterSwitchController : MonoBehaviour
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
     }
     private void Update()
     {
@@ -32,14 +35,21 @@ public class CharacterSwitchController : MonoBehaviour
     {
         if (index == 0)
         {
-            player.GetComponent<SpriteRenderer>().color = Color.red;
-            
+            //player.GetComponent<SpriteRenderer>().color = Color.red;
+            evilPlayer = Instantiate(player);
+            evilPlayer.GetComponent<SpriteRenderer>().color = Color.red;
+            mainCamera.GetComponent<CameraFollow>().target = evilPlayer.transform;
+            player.GetComponent<PlayerController>().enabled = false;
+            player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
             index = 1;
         }
         else if (index == 1)
         {
-            player.GetComponent<SpriteRenderer>().color = Color.black;
-
+            //player.GetComponent<SpriteRenderer>().color = Color.black;
+            Destroy(evilPlayer);
+            mainCamera.GetComponent<CameraFollow>().target = player.transform;
+            player.GetComponent<PlayerController>().enabled = true;
+            player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
             index = 0;
         }
        
