da36b49bc40ece67b867eb135113fdff163b6744
bince25 <incebaran25@gmail.com>
2022-07-15 14:18:57 +0300

    MovementRestriction
    Karakterler arası ruh bağını göstermedim daha orası karışık duruyor. Bozmadan önce çalışan halini göndereyim dedim :D

--- Assets/Scripts/CharacterSwitchController.cs
+++ Assets/Scripts/CharacterSwitchController.cs
@@ -10,6 +10,7 @@ public class CharacterSwitchController : MonoBehaviour
 
     public GameObject NormalMap;
     public GameObject EvilMap;
+    public float maxDistanceFromPlayer;
 
     [HideInInspector]
     public int index = 0; // 0 --> normal mod, 1 --> evil mod
@@ -30,6 +31,13 @@ public class CharacterSwitchController : MonoBehaviour
             NormalMap.SetActive(false);
             EvilMap.SetActive(true);
         }
+
+        if (evilPlayer)
+        {
+            EvilPlayerMovementRestriction();
+            Debug.Log(Vector3.Distance(evilPlayer.transform.position, player.transform.position));
+        }
+
     }
     public void SwitchCharacter()
     {
@@ -40,7 +48,10 @@ public class CharacterSwitchController : MonoBehaviour
             evilPlayer.GetComponent<SpriteRenderer>().color = Color.red;
             mainCamera.GetComponent<CameraFollow>().target = evilPlayer.transform;
             player.GetComponent<PlayerController>().enabled = false;
+            player.GetComponent<BoxCollider2D>().isTrigger = true;
             player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+
+
             index = 1;
         }
         else if (index == 1)
@@ -49,9 +60,34 @@ public class CharacterSwitchController : MonoBehaviour
             Destroy(evilPlayer);
             mainCamera.GetComponent<CameraFollow>().target = player.transform;
             player.GetComponent<PlayerController>().enabled = true;
+            player.GetComponent<BoxCollider2D>().isTrigger = false;
             player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
             index = 0;
         }
        
+    }
+    void EvilPlayerMovementRestriction()
+    {
+        float rightRestriction = player.transform.position.x + maxDistanceFromPlayer;
+        float leftRestriction = player.transform.position.x - maxDistanceFromPlayer;
+        float upRestriction = player.transform.position.y + maxDistanceFromPlayer;
+        float downRestriction = player.transform.position.y - maxDistanceFromPlayer;
+
+        if (evilPlayer.transform.position.x > rightRestriction)
+        {
+            evilPlayer.transform.position = new Vector3(rightRestriction, evilPlayer.transform.position.y, evilPlayer.transform.position.z);
+        }
+        else if (evilPlayer.transform.position.x < leftRestriction)
+        {
+            evilPlayer.transform.position = new Vector3(leftRestriction, evilPlayer.transform.position.y, evilPlayer.transform.position.z);
+        }
+        if (evilPlayer.transform.position.y > upRestriction)
+        {
+            evilPlayer.transform.position = new Vector3(evilPlayer.transform.position.x, upRestriction, evilPlayer.transform.position.z);
+        }
+        else if (evilPlayer.transform.position.y < downRestriction)
+        {
+            evilPlayer.transform.position = new Vector3(evilPlayer.transform.position.x, downRestriction, evilPlayer.transform.position.z);
+        }
     }
 }
