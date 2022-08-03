de7af8209b2ba0590ec2443436161dea6d92735f
Yakup Erdem Yılmaz <yakuperdemyilmaz@outlook.com>
2022-08-03 11:19:13 +0300

    Yeni level (bitmedi)

---
+++ Assets/Scripts/Environment/MovingSpike.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSpike : MonoBehaviour
+{
+    public GameObject platform;
+    public Transform up, down;
+    public float speed;
+    private float _direction = 1;
+    private void Update()
+    {
+        DirectionCheck();
+        platform.transform.position += Vector3.up * (speed * Time.deltaTime * _direction);
+    }
+
+    private void DirectionCheck()
+    {
+        if (platform.transform.position.y > up.position.y)
+        {
+            _direction = -1;
+        }
+        else if (platform.transform.position.y < down.position.y)
+        {
+            _direction = 1;
+        }
+    }
+}
