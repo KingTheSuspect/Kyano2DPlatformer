7388650b4b2bcfcfcd1bd74aad93cab6225cca9f
ArdaTheMusician <83888052+ArdaTheMusician@users.noreply.github.com>
2022-10-15 18:51:39 +0300

    .

--- Assets/Scripts/TouchbleBox.cs
+++ Assets/Scripts/TouchbleBox.cs
@@ -4,49 +4,21 @@ using UnityEngine;
 
 public class TouchbleBox : MonoBehaviour
 {   
+private void OnTriggerEnter2D(Collider2D col)
+	{
 
-    public float horizontalSpeed = 1f;
-    PlayerController1 player;
-    Rigidbody2D boxBody;
-    Collider2D colider;
-    bool isGettingPushed = false;
- 
-    void Awake()
-    {
-        boxBody = transform.parent.GetComponent<Rigidbody2D>();
-    }
- 
-    void FixedUpdate()
-    {
-        if(isGettingPushed)
-        {
-            Vector2 velocity = boxBody.velocity;
-            velocity.x = horizontalSpeed;
-            boxBody.velocity = velocity;
-            if (player.facingRight)
-                boxBody.velocity = velocity * player.speed;
-            else
-                boxBody.velocity = -velocity * player.speed;
-        }
+		if (col.gameObject.CompareTag("Button"))
+		{
+			col.gameObject.GetComponent<IButtonInteract>().Interact(true);
+		}
+	}
 
-
-    }
- 
-    void OnTriggerEnter2D(Collider2D collider)
-    {
-        if (collider != null && collider.CompareTag("Player"))
-        {
-            player = collider.gameObject.GetComponent<PlayerController1>();
-            isGettingPushed = true;
-        }
-    }
- 
-    void OnTriggerExit2D(Collider2D collider)
-    {
-        if (collider != null && collider.CompareTag("Player"))
-        {
-            isGettingPushed = false;
-        }
-    }
+	private void OnTriggerExit2D(Collider2D col)
+	{
+		if (col.gameObject.CompareTag("Button"))
+		{
+			col.gameObject.GetComponent<IButtonInteract>().Interact(false);
+		}
+	}
 }
 
